e526b248d19d447d86c789524a0e47aed774b7c6
Georg Kemser <g.kemser@minnt.com>
2021-11-18 15:32:15 +0100

    Initial Commit

---
+++ Playlist Export/Playlist_ExportApp.swift
@@ -0,0 +1,17 @@
+//
+//  Playlist_ExportApp.swift
+//  Playlist Export
+//
+//  Created by Georg Kemser on 18.11.21.
+//
+
+import SwiftUI
+
+@main
+struct Playlist_ExportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
